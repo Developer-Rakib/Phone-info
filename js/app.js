bd5b46dca44b2960445356f67128ff8133759f74
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-02-28 23:50:27 +0600

    disply data

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ let inputFeild = document.getElementById("input-feild");
 let load = document.getElementById("loading");
 
 
-let url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+let url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`
 fetch(url)
     .then(res => res.json())
     .then(data => {
-        console.log(data.data[0]);
+        console.log(data.data);
     })
 
 document.getElementById("search-btn").addEventListener("click", () => {
@@ -54,7 +54,6 @@ function displyData(datas) {
     } else {
         let data20 = datas.slice(0, 20);
         data20.forEach(data => {
-            // console.log(data);
             let div = document.createElement("div");
             div.classList.add("phones-div")
             div.innerHTML = `
@@ -62,7 +61,7 @@ function displyData(datas) {
                             <h5>Phone Name : ${data.phone_name}</h5>
                             <p>Brand : ${data.brand}</p>
                             <p>Phone Code : ${data.slug}</p>
-                            <button onclick="displySinglePhoen('${data.slug}')">Tap More Details</button>
+                            <button onclick="leadSinglePhone('${data.slug}')">Tap More Details</button>
 
                 `;
             phones.appendChild(div);
@@ -74,27 +73,62 @@ function displyData(datas) {
 
 }
 
-function displySinglePhoen(PhoneCode) {
+function leadSinglePhone(PhoneCode) {
 
-    console.log(PhoneCode);
-    // phone.textContent = "";
-    // let TeamDiv = document.createElement("div");
+    let url = `https://openapi.programming-hero.com/api/phone/${PhoneCode}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displySinglePhone(data.data))
+}
+function displySinglePhone(data) {
+    console.log(data)
+    phone.textContent = "";
+    let singlePhone = document.createElement("div");
+    singlePhone.classList.add("single-phone");
+    singlePhone.innerHTML = `
 
-    // console.log(data);
-    // TeamDiv.innerHTML = `
+                    <div class="img-div"><img src="${data.image}" alt=""></div>
+                    <h5>Phone Name : ${data.name}</h5>
+                    <p>Brand : ${data.brand}</p>
+                    <p>Phone Code : ${data.slug}</p>
+                    <p>Release Date : ${data.releaseDate}</p>
+                    <p>Main Features</p>
+                    <ul id="mainfeatures">
+                        <li>Storage : ${data.mainFeatures.storage}</li>
+                        <li>display Size : ${data.mainFeatures.displaySize}</li>
+                        <li>Memory : ${data.mainFeatures.memory}</li>
+                        <li>ChipSet : ${data.mainFeatures.chipSet}</li>
+                        <li>Sensors : 
+                            <ul id="sensor">
+                                <li>${data.mainFeatures.sensors[0]}</li>
+                                <li>${data.mainFeatures.sensors[1]}</li>
+                                <li>${data.mainFeatures.sensors[2]}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    
+        `;
 
-    //                 <img src="${data.strTeamBadge}" alt="">
-    //                 <h3> ${data.strTeam}</h3>
-    //                 <p>Team Id : ${data.idTeam}</p>
-    //                 <p>Formed Year : ${data.intFormedYear}</p>
-    //                 <p>Sports Type : ${data.strSport}</p>
-    //                 <p>Team Gender : ${data.strGender}</p>
-    //                 <p>League : ${data.strLeague}</p>
-    //                 <p>Stadium : ${data.strStadium}</p>
-    //                 <p>Description : ${data.strDescriptionEN.slice(0, 150)}</p>
+    phone.appendChild(singlePhone);
 
-    //     `;
-    // phone.appendChild(TeamDiv);
+    if (data.others != undefined) {
+        let div = document.createElement("div");
+        div.innerHTML = `
+                            <ul id="others">
+                                <p>Others</p>
+                                <li>Bluetooth : ${data.others?.Bluetooth}</li>
+                                <li>WLAN : ${data.others?.WLAN}</li>
+                                <li>USB : ${data.others?.USB}</li>
+                                <li>GPS : ${data.others?.GPS}</li>
+                                <li>NFC : ${data.others?.NFC}</li>
+                                <li>Radio : ${data.others?.Radio}</li>
+                            </ul>
+            
+            `;
+        singlePhone.appendChild(div);
+
+
+    }
     // phones.setAttribute("for", "team");
 
 
