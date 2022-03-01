2d8b8eacc8527500093ce8e8ae0e80190814ffc9
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-03-01 11:28:51 +0600

    error done

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ let inputFeild = document.getElementById("input-feild");
 let load = document.getElementById("loading");
 
 
-// let url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`
+// let url = `https://openapi.programming-hero.com/api/phones?search`
 // fetch(url)
 //     .then(res => res.json())
 //     .then(data => {
-//         console.log(data.data);
+//         data.data.forEach(a => console.log(a.brand))
 //     })
 
 document.getElementById("search-btn").addEventListener("click", () => {
@@ -49,7 +49,7 @@ document.getElementById("search-btn").addEventListener("click", () => {
 })
 
 
-function displyData(datas) {
+const displyData = datas => {
 
     let data20 = datas.slice(0, 20);
     data20.forEach(data => {
@@ -59,8 +59,7 @@ function displyData(datas) {
                         <div><img src="${data.image}" alt=""></div>
                         <h5>Phone Name : ${data.phone_name}</h5>
                         <p>Brand : ${data.brand}</p>
-                        <p>Phone Code : ${data.slug}</p>
-                        <button onclick="leadSinglePhone('${data.slug}')">Tap More Details</button>
+                        <button onclick="leadSinglePhone('${data.slug}')">Explore Now</button>
 
             `;
         phones.appendChild(div);
@@ -68,7 +67,7 @@ function displyData(datas) {
 
 }
 
-function leadSinglePhone(PhoneCode) {
+const leadSinglePhone = PhoneCode => {
     load.style.display = "block";
     let url = `https://openapi.programming-hero.com/api/phone/${PhoneCode}`;
     fetch(url)
@@ -84,26 +83,27 @@ function leadSinglePhone(PhoneCode) {
             }, 500);
         })
 }
-function displySinglePhone(data) {
-    console.log(data)
+const displySinglePhone = data => {
     phone.textContent = "";
     let singlePhone = document.createElement("div");
     singlePhone.classList.add("single-phone");
+    if (data.releaseDate == "") {
+        data.releaseDate = "Release date not found"
+    }
     singlePhone.innerHTML = `
 
                     <div class="img-div"><img src="${data.image}" alt=""></div>
                     <h5>Phone Name : ${data.name}</h5>
-                    <p>Brand : ${data.brand}</p>
-                    <p>Phone Code : ${data.slug}</p>
                     <p>Release Date : ${data.releaseDate}</p>
-                    <p>Main Features</p>
+                    <p>Brand : ${data.brand}</p>
+                    <p>Main Features :</p>
                     <ul id="mainfeatures">
                         <li>Storage : ${data.mainFeatures.storage}</li>
                         <li>display Size : ${data.mainFeatures.displaySize}</li>
                         <li>Memory : ${data.mainFeatures.memory}</li>
                         <li>ChipSet : ${data.mainFeatures.chipSet}</li>
                         <li>Sensors : 
-                            <ul id="sensor">
+                            <ul id="sensor :">
                                 <li>${data.mainFeatures.sensors[0]}</li>
                                 <li>${data.mainFeatures.sensors[1]}</li>
                                 <li>${data.mainFeatures.sensors[2]}</li>
@@ -118,8 +118,8 @@ function displySinglePhone(data) {
     if (data.others != undefined) {
         let div = document.createElement("div");
         div.innerHTML = `
+                            <p>Others :</p>
                             <ul id="others">
-                                <p>Others</p>
                                 <li>Bluetooth : ${data.others?.Bluetooth}</li>
                                 <li>WLAN : ${data.others?.WLAN}</li>
                                 <li>USB : ${data.others?.USB}</li>
@@ -131,7 +131,6 @@ function displySinglePhone(data) {
             `;
         singlePhone.appendChild(div);
     }
-    // phones.setAttribute("for", "team");
 
 
 }
